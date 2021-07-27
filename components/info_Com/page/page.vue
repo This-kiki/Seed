909acfdb7337f72e9bb3b6df9308210f82859ab8
kiki <1417863646@qq.com>
2021-07-27 12:04:13 +0800

    update
    资讯页面架构

---
+++ components/info_Com/page/page.vue
@@ -0,0 +1,95 @@
+<template>
+	<view>
+		<loading ref="loadRefresh" :currentPage="currentPage" :totalPages="totalPages" @loadMore="loadMore" @refresh="refresh">
+			<view slot="content-list">
+				<!-- 数据列表 -->
+				<view v-for="(item, index) in list" :key="index">
+					<news-card :options="item"></news-card>
+				</view>
+			</view>
+		</loading>
+	</view>
+</template>
+
+<script>
+import loading from '@/components/info_Com/load-refresh/load-refresh.vue';
+import newsCard from '@/components/info_Com/newsCard/index.vue';
+export default {
+	components: {
+		loading,
+		newsCard
+	},
+	data() {
+		return {
+			list: [
+				{
+					img: '/static/hm-news-card/images/img_22726_0_0.png',
+					title: '新闻标题',
+					author: '作者',
+					date: '16 MAY 2016',
+					summary:
+					'新闻概要。新闻概要长度最好控制在50字符以内',
+					comments: "14 评论",
+					likes: "254 喜欢",
+					url: '',
+					showComments: true,
+					showLikes: true
+				},{
+					img: '/static/hm-news-card/images/img_22726_0_0.png',
+					title: '新闻标题',
+					author: '作者',
+					date: '16 MAY 2016',
+					summary:
+					'新闻概要。新闻概要长度最好控制在50字符以内',
+					comments: "14 评论",
+					likes: "254 喜欢",
+					url: '',
+					showComments: true,
+					showLikes: true
+				},{
+					img: '/static/hm-news-card/images/img_22726_0_0.png',
+					title: '新闻标题',
+					author: '作者',
+					date: '16 MAY 2016',
+					summary:
+					'新闻概要。新闻概要长度最好控制在50字符以内',
+					comments: "14 评论",
+					likes: "254 喜欢",
+					url: '',
+					showComments: true,
+					showLikes: true
+				}
+			], // 数据集
+			currentPage: 1, // 当前页码
+			totalPages: 2 // 总页数
+		};
+	},
+	methods: {
+		// 上划加载更多
+		loadMore() {
+			// 模拟请求成功后的回调
+			setTimeout(() => {
+				console.log('加载更多')
+				// 1. list数组添加新数据
+				// 2. 更新当前页码 currentPage
+				// 3. 调用completed()方法 this.$refs.loadRefresh.completed()
+			}, 800)
+		  },
+		// 下拉刷新数据列表
+		refresh() {
+			// 模拟请求成功后的回调
+			setTimeout(() => {
+				// 1. list重新赋值，应避免 this.list = [] 这种操作
+				// 2. 更新当前页码 currentPage
+				// 3. 调用completed()方法 this.$refs.loadRefresh.completed()
+			}, 1600)
+		},
+		// 代码触发下拉刷新方法
+		runRefresh() {
+			this.$refs.loadRefresh.runRefresh()
+		}
+	}
+};
+</script>
+
+<style></style>
--- pages/RealTimeInfo/RealTimeInfo.vue
+++ pages/RealTimeInfo/RealTimeInfo.vue
@@ -1,22 +1,41 @@
 <template>
 	<view>
-		资讯
+		<view class="top-nav-bar">
+			<view style="margin-top: 20upx;"><top-nav-bar :items="items" :current="current" @clickItem="onClickItem" ref="mytabs"></top-nav-bar></view>
+			<view style="margin-top: 20upx;color:#999999;font-size: 24upx;height: 260upx;">
+				<view v-show="current === 0">
+					<page></page>
+				</view>
+				<view v-show="current === 1">会员风采</view>
+				<view v-show="current === 2">会员单位</view>
+				<view v-show="current === 3">家乡新闻</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
+import topNavBar from '@/components/info_Com/topNavBar/zzx-tabs.vue';
+import Page from '@/components/info_Com/page/page.vue';
+export default {
+	components: {
+		topNavBar,
+		Page
+	},
+	data() {
+		return {
+			items: ['种子会动态', '会员风采', '会员单位', '家乡新闻'],
+			current: 0
+		};
+	},
+	methods: {
+		onClickItem(e) {
+			if (this.current !== e.currentIndex) {
+				this.current = e.currentIndex;
 			}
-		},
-		methods: {
-			
 		}
 	}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
