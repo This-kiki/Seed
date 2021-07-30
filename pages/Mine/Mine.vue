2852fe688025c7ccb1f3a5b5557846caafe96fdc
kiki <1417863646@qq.com>
2021-07-30 20:59:45 +0800

    update
    我的页面

--- pages/Mine/Mine.vue
+++ pages/Mine/Mine.vue
@@ -1,6 +1,83 @@
 <template>
-	<view>
-		我的
+	<view class="mine-page">
+		<view class="m-page">
+			<view class="m-page-top">
+				<view class="m-page-tag">
+					
+				</view>
+				<view class="m-page-headMsg">
+					<image class="m-page-headimg" src="" mode=""></image>
+					<view class="m-page-name">
+						
+					</view>
+				</view>
+				<view class="m-page-join">
+					<view class="m-page-jointitle">
+						<image class="m-page-joinimg" src="" mode=""></image>
+						<view class="m-page-jointext">
+							
+						</view>
+					</view>
+					<view class="m-page-jointile">
+						<view class="m-page-jointext">
+							
+						</view>
+						<image class="m-page-joinicon" src="" mode=""></image>
+					</view>
+				</view>
+			</view>
+			<view class="m-page-middle">
+				<view class="m-page-box">
+					<image class="m-page-boximg" src="" mode=""></image>
+					<view class="m-page-boxtext">
+						
+					</view>
+				</view>
+				<view class="m-page-box">
+					<image class="m-page-boximg" src="" mode=""></image>
+					<view class="m-page-boxtext">
+						
+					</view>
+				</view>
+				<view class="m-page-box">
+					<image class="m-page-boximg" src="" mode=""></image>
+					<view class="m-page-boxtext">
+						
+					</view>
+				</view>
+			</view>
+			<view class="m-page-bottom">
+				<view class="m-page-list">
+					<view class="m-page-listitem">
+						<view class="m-page-listitemtitle">
+							<image class="m-page-listitemimg" src="" mode=""></image>
+							<view class="m-page-listitemtext">
+								
+							</view>
+						</view>
+						<image class="m-page-listitemicon" src="" mode=""></image>
+					</view>
+					<view class="m-page-listitem">
+						<view class="m-page-listitemtitle">
+							<image class="m-page-listitemimg" src="" mode=""></image>
+							<view class="m-page-listitemtext">
+								
+							</view>
+						</view>
+						<image class="m-page-listitemicon" src="" mode=""></image>
+					</view>
+					<view class="m-page-listitem">
+						<view class="m-page-listitemtitle">
+							<image class="m-page-listitemimg" src="" mode=""></image>
+							<view class="m-page-listitemtext">
+								
+							</view>
+						</view>
+						<image class="m-page-listitemicon" src="" mode=""></image>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
