879efb606bb77479cd74051be06df5e1880951bb
kiki <1417863646@qq.com>
2021-08-02 20:24:23 +0800

    update
    管理端完成请求封装，路由跳转，登录

--- CommerceBackstage/src/main.js
+++ CommerceBackstage/src/main.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
+import App from './App.vue'
 
 // Element UI组件
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 Vue.use(ElementUI);
 
-import App from './App.vue'
+// 权限控制
+import './permission.js'
 
 // Router
-import VueRouter from 'vue-router'
-import routerMap from './router/router.js'
-Vue.use(VueRouter);
+// import VueRouter from 'vue-router'
+import router from './router/router.js'
+// Vue.use(VueRouter);
+// const router = new VueRouter({routes: routerMap})
 
 // vuex
 import store from './store/index'
@@ -19,9 +22,8 @@ import store from './store/index'
 import http from './api/http.js'
 Vue.prototype.$http = http
 
-const router = new VueRouter({routes: routerMap})
 
 const app = new Vue({
   store,
-  router
+  router,
 }).$mount('#app');
