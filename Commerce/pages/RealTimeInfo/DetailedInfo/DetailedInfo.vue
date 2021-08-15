9ced6dcb1ede38954e0308b7e02581c4a83cce3c
渊中月 <lq193979@163.com>
2021-08-15 19:40:37 +0800

    11

---
+++ Commerce/pages/RealTimeInfo/DetailedInfo/DetailedInfo.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+		<view class="info-title">
+			{{dataForm.title}}
+		</view>
+		<view class="info-content" v-html="dataForm.content">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:["infoId"],
+		data() {
+			return {
+				dataForm: null
+			}
+		},
+		mounted() {
+			this.getInfo()
+		},
+		methods: {
+			getInfo() {
+				var getAPI = {id: this.infoId}
+				this.$api.getOneInfo(getAPI).then((res) => {
+					this.dataForm = res.data.Info
+					// console.log(res.data.Info)
+				})
+			}
+		}
+	}
+</script>
+
+<style scoped>
+.info-title {
+	margin: 20rpx 10rpx;
+	font-size: 35rpx;
+	font-weight: 900;
+}
+.info-content {
+	padding: 20rpx;
+}
+</style>
