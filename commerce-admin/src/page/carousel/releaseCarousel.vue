9ff555f94997981918759f6244195b281f080959
渊中月 <lq193979@163.com>
2021-08-31 22:39:38 +0800

    0101

--- commerce-admin/src/page/carousel/releaseCarousel.vue
+++ commerce-admin/src/page/carousel/releaseCarousel.vue
@@ -4,23 +4,25 @@
       <el-row>
         <el-col :span="2" :offset="22">
           <div style="margin: 10px 0">
-            <el-button type="success" @click="submit" icon="el-icon-plus" size="small" plain>
-              提 交</el-button>
+            <el-button
+              type="success"
+              @click="submit"
+              icon="el-icon-plus"
+              size="small"
+              plain
+            >
+              提 交</el-button
+            >
           </div>
         </el-col>
         <el-col :span="8">
           <el-form ref="form" :model="addForm" label-width="120px">
-            <el-form-item label="轮播图位置">
-              <el-select v-model="position" placeholder="请选择轮播图位置">
-                <el-option v-for="item in typeList" :key="item.id" :label="item.name" :value="item.id">
-                </el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="资讯id" v-if="position == 1">
-              <el-input v-model="addForm.infoId" placeholder="请从资讯列表处复制资讯id到此处" style="width: 300px"></el-input>
-            </el-form-item>
             <el-form-item label="图片链接">
-              <el-input v-model="addForm.img" placeholder="请在下方上传图片并将图片链接复制到此处" style="width: 300px"></el-input>
+              <el-input
+                v-model="addForm.img"
+                placeholder="请在下方上传图片并将图片链接复制到此处"
+                style="width: 300px"
+              ></el-input>
             </el-form-item>
           </el-form>
         </el-col>
@@ -30,9 +32,24 @@
       <el-row style="margin: 10px 0">
         <el-col :span="2" :offset="22">
           <div>
-            <input type="file" name="avatar" accept="image/gif,image/jpeg,image/jpg,image/png" style="display: none" @change="changeImage($event)" ref="avatarInput" />
-            <el-button type="primary" @click="uploadImg" icon="el-icon-upload" round size="small" plain>
-              上传图片</el-button>
+            <input
+              type="file"
+              name="avatar"
+              accept="image/gif,image/jpeg,image/jpg,image/png"
+              style="display: none"
+              @change="changeImage($event)"
+              ref="avatarInput"
+            />
+            <el-button
+              type="primary"
+              @click="uploadImg"
+              icon="el-icon-upload"
+              round
+              size="small"
+              plain
+            >
+              上传图片</el-button
+            >
           </div>
         </el-col>
       </el-row>
@@ -46,7 +63,14 @@
           <el-table-column prop="img" label="图片地址"> </el-table-column>
           <el-table-column fixed="right" label="操作" width="80" align="center">
             <template slot-scope="scope">
-              <el-button type="primary" @click="copyUrl(scope.row)" plain circle icon="el-icon-view" size="small"></el-button>
+              <el-button
+                type="primary"
+                @click="copyUrl(scope.row)"
+                plain
+                circle
+                icon="el-icon-view"
+                size="small"
+              ></el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -60,74 +84,57 @@ export default {
   data() {
     return {
       addForm: {
-        infoId: '',
-        img: '',
+        infoId: "",
+        img: "",
       },
-      imgList: [{ img: '1111' }],
-      position: 1,
-      typeList: [
-        { id: 1, name: '首页' },
-        { id: 2, name: '种子会' },
-      ],
-    }
+      imgList: [],
+    };
   },
   methods: {
     copyUrl(row) {
-      var _th = this
-      var input = document.getElementById('input')
-      input.value = row.img // 修改文本框的内容
-      input.select() // 选中文本
-      document.execCommand('Copy')
+      var _th = this;
+      var input = document.getElementById("input");
+      input.value = row.img; // 修改文本框的内容
+      input.select(); // 选中文本
+      document.execCommand("Copy");
       _th.$notify({
-        message: '已复制到剪切板',
-        type: 'success',
-      })
+        message: "已复制到剪切板",
+        type: "success",
+      });
     },
     uploadImg() {
-      this.$refs.avatarInput.click()
+      this.$refs.avatarInput.click();
     },
     changeImage(e) {
-      var file = e.target.files[0]
-      var reader = new FileReader()
-      var _this = this
-      reader.readAsDataURL(file)
+      var file = e.target.files[0];
+      var reader = new FileReader();
+      var _this = this;
+      reader.readAsDataURL(file);
       reader.onload = function (e) {
         // that.avatar = this.result
         if (_this.$refs.avatarInput.files.length !== 0) {
-          var image = new FormData()
-          image.append('file', _this.$refs.avatarInput.files[0])
+          var image = new FormData();
+          image.append("file", _this.$refs.avatarInput.files[0]);
           _this.$http.uploadImg(image).then((res) => {
             // console.log(res)
-            _this.imgList.push({ img: res.data.url })
-          })
+            _this.imgList.push({ img: res.data.url });
+          });
         }
-      }
+      };
     },
     submit() {
-      if (this.position == 1) {
-        this.$http.uploadHomeCarousel(this.addForm).then((res) => {
-          // console.log(res)
-          if (res.code == 20000) {
-            this.$message({
-              message: '首页轮播图发布成功',
-              type: 'success',
-            })
-          }
-        })
-      } else {
-        this.$http.uploadSeedCarousel(this.addForm).then((res) => {
-          // console.log(res)
-          if (res.code == 20000) {
-            this.$message({
-              message: '种子会轮播图发布成功',
-              type: 'success',
-            })
-          }
-        })
-      }
+      this.$http.uploadSeedCarousel(this.addForm).then((res) => {
+        // console.log(res)
+        if (res.code == 20000) {
+          this.$message({
+            message: "种子会轮播图发布成功",
+            type: "success",
+          });
+        }
+      });
     },
   },
-}
+};
 </script>
 <style scoped>
 .hid {
