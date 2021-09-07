8a10eea3b41a62fcc30b669b4d5a9c970478a6ff
渊中月 <lq193979@163.com>
2021-09-07 14:41:42 +0800

    01010

--- Commerce/pages/Map/Map.vue
+++ Commerce/pages/Map/Map.vue
@@ -2,7 +2,8 @@
 	<view>
 		<!-- 顶部 -->
 		<topBar :nav="setNav"></topBar>
-		<map style="width: 100%; height: 100vh;" :latitude="latitude" :longitude="longitude" :markers="covers" :scale ="scale"></map>
+		<map style="width: 100%; height: 100vh;" :latitude="latitude" :longitude="longitude" :markers="covers"
+			:scale="scale"></map>
 	</view>
 </template>
 
@@ -17,14 +18,16 @@
 					isShowBackBtn: true,
 					backBtnColor: "black"
 				},
-				latitude: 23.293258753246587,
-				longitude: 116.6903593331881,
+				latitude: 23.293167,
+				longitude: 116.690369,
 				covers: [{
-					id:0,
-					latitude: 23.293258753246587,
-					longitude: 116.6903593331881,
+					id: 0,
+					latitude: 23.293167,
+					longitude: 116.690369,
 					title: "汕头市濠江区珠浦社区康乐活动中心",
 					iconPath: '../../static/icon/location.png',
+					width: 30,
+					height: 40
 				}],
 				scale: 18
 			};
