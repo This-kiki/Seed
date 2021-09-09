8e01443f347919a9810086fa8de361c470661dbe
渊中月 <lq193979@163.com>
2021-09-09 23:08:35 +0800

    1212

--- Commerce/pages/Mine/MineMsg/MineMsg.vue
+++ Commerce/pages/Mine/MineMsg/MineMsg.vue
@@ -5,7 +5,10 @@
 		<view class="container">
 			<view class="ui-all">
 				<view class="avatar" @tap="avatarChoose">
-					<view class="imgAvatar"><view class="iavatar" :style="'background: url(' + mineMsg.img + ') no-repeat center/cover #eeeeee;'"></view></view>
+					<view class="imgAvatar">
+						<view class="iavatar"
+							:style="'background: url(' + mineMsg.img + ') no-repeat center/cover #eeeeee;'"></view>
+					</view>
 					<text v-if="mineMsg.img">修改头像</text>
 					<text v-if="!mineMsg.img">选择头像</text>
 				</view>
@@ -21,7 +24,8 @@
 				</view>
 				<view class="picker-box">
 					<text>性别</text>
-					<picker class="picker" mode="selector" range-key="name" v-model="mineMsg.sex" @change="bindSexChange" :range="sex">
+					<picker class="picker" mode="selector" range-key="name" v-model="mineMsg.sex"
+						@change="bindSexChange" :range="sex">
 						<view class="picker-text">{{ mineMsg.sex == 0 ? '男' : mineMsg.sex == 1 ? '女' : '' }}</view>
 					</picker>
 				</view>
@@ -34,6 +38,14 @@
 				<view class="text-box">
 					<text>手机号</text>
 					<input class="input-box" type="text" v-model="mineMsg.phone" placeholder-class="place" />
+				</view>
+				<view class="text-box">
+					<text>学校</text>
+					<input class="input-box" type="text" v-model="mineMsg.school" placeholder-class="place" />
+				</view>
+				<view class="text-box">
+					<text>专业</text>
+					<input class="input-box" type="text" v-model="mineMsg.major" placeholder-class="place" />
 				</view>
 				<view class="text-box">
 					<text>年级</text>
@@ -54,330 +66,337 @@
 </template>
 
 <script>
-import { regular } from '../../../util/common.js';
-export default {
-	data() {
-		return {
-			setNav: {
-				titleColor: 'black',
-				navTitle: '个人资料',
-				bgColor: 'white',
-				isShowBackBtn: true,
-				backBtnColor: 'black'
-			},
-			temp: null,
-			mineMsg: {
-				img: '',
-				name: '',
-				birth: '',
-				sex: 3,
-				place: '',
-				phone: '',
-				grade: '',
-				work: '',
-				position: ''
-			},
-			sex: [
-				{
-					id: 0,
-					name: '男'
+	import {
+		regular
+	} from '../../../util/common.js';
+	export default {
+		data() {
+			return {
+				setNav: {
+					titleColor: 'black',
+					navTitle: '个人资料',
+					bgColor: 'white',
+					isShowBackBtn: true,
+					backBtnColor: 'black'
 				},
-				{
-					id: 1,
-					name: '女'
-				}
-			]
-		};
-	},
-	mounted() {
-		this.getUserInfo();
-	},
-	methods: {
-		judge() {
-			// console.log(regular(this.mineMsg))
-			return regular(this.mineMsg);
+				temp: null,
+				mineMsg: {
+					img: '',
+					name: '',
+					birth: '',
+					sex: 3,
+					place: '',
+					phone: '',
+					school: '',
+					major: '',
+					grade: '',
+					work: '',
+					position: ''
+				},
+				sex: [{
+						id: 0,
+						name: '男'
+					},
+					{
+						id: 1,
+						name: '女'
+					}
+				]
+			};
 		},
-		getUserInfo() {
-			this.$api.getUserMsg().then(res => {
-				// this.mineMsg = res.data.userBaseInfo;
-				var obj = res.data.userBaseInfo;
-				for (let key in obj) {
-					// console.log(key,'-',obj[key])
-					this.mineMsg[key] = obj[key];
-				}
-			});
+		mounted() {
+			this.getUserInfo();
 		},
-		bindSexChange(e) {
-			this.mineMsg.sex = e.detail.value;
-		},
-		bindDateChange(e) {
-			this.mineMsg.birth = e.detail.value;
-		},
-		bindRegionChange(e) {
-			// console.log(e)
-			this.mineMsg.place = e.detail.value.join('-');
-		},
-		save() {
-			// if (this.judge().status == true) {
-			if (!this.mineMsg.name) {
-				uni.showToast({
-					title: '请填写姓名',
-					icon: 'none'
+		methods: {
+			judge() {
+				// console.log(regular(this.mineMsg))
+				return regular(this.mineMsg);
+			},
+			getUserInfo() {
+				this.$api.getUserMsg().then(res => {
+					// this.mineMsg = res.data.userBaseInfo;
+					var obj = res.data.userBaseInfo;
+					for (let key in obj) {
+						// console.log(key,'-',obj[key])
+						this.mineMsg[key] = obj[key];
+					}
 				});
-				return false;
-			}
-			uni.showLoading({
-				title: '正在提交'
-			});
-			if (this.temp) {
-				this.$api
-					.uploadPicture({
-						tempFilePaths: this.temp
-					})
-					.then(res => {
-						// console.log(res)
-						var obj = this.mineMsg;
-						obj.img = res.data.url;
-						this.$api
-							.changeUserMsg(obj)
-							.then(res => {
-								uni.hideLoading();
-								if (res.code == 20000) {
-									uni.showToast({
-										title: '修改成功',
-										duration: 2000
-									});
-									setTimeout(() => {
-										uni.navigateBack({});
-									}, 1000);
-								}
-							})
-							.catch(err => {
-								uni.hideLoading();
-								uni.showToast({
-									title: '提交失败，请重新提交',
-									icon: 'none'
-								});
-							});
+			},
+			bindSexChange(e) {
+				this.mineMsg.sex = e.detail.value;
+			},
+			bindDateChange(e) {
+				this.mineMsg.birth = e.detail.value;
+			},
+			bindRegionChange(e) {
+				// console.log(e)
+				this.mineMsg.place = e.detail.value.join('-');
+			},
+			save() {
+				// if (this.judge().status == true) {
+				if (!this.mineMsg.name) {
+					uni.showToast({
+						title: '请填写姓名',
+						icon: 'none'
 					});
-			} else {
-				var obj = this.mineMsg;
-				this.$api
-					.changeUserMsg(obj)
-					.then(res => {
-						uni.hideLoading();
-						if (res.code == 20000) {
-							uni.showToast({
-								title: '修改成功',
-								duration: 2000
-							});
-							setTimeout(() => {
-								uni.navigateBack({});
-							}, 1000);
-						}
-					})
-					.catch(err => {
-						uni.hideLoading();
-						uni.showToast({
-							title: '提交失败，请重新提交',
-							icon: 'none'
-						});
-					});
-			}
-			// } else {
-			// 	uni.showToast({
-			// 		title: this.judge().value,
-			// 		icon: 'none'
-			// 	});
-			// 	return false;
-			// }
-		},
-		isPoneAvailable(poneInput) {
-			var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
-			if (!myreg.test(poneInput)) {
-				return false;
-			} else {
-				return true;
-			}
-		},
-		avatarChoose() {
-			let that = this;
-			uni.chooseImage({
-				count: 1,
-				sizeType: ['original', 'compressed'],
-				sourceType: ['album', 'camera'],
-				success(res) {
-					const tempFilePaths = res.tempFilePaths;
-					that.temp = tempFilePaths;
-					that.mineMsg.img = tempFilePaths[0];
+					return false;
 				}
-			});
+				uni.showLoading({
+					title: '正在提交'
+				});
+				if (this.temp) {
+					this.$api
+						.uploadPicture({
+							tempFilePaths: this.temp
+						})
+						.then(res => {
+							// console.log(res)
+							var obj = this.mineMsg;
+							obj.img = res.data.url;
+							this.$api
+								.changeUserMsg(obj)
+								.then(res => {
+									uni.hideLoading();
+									if (res.code == 20000) {
+										uni.showToast({
+											title: '修改成功',
+											duration: 2000
+										});
+										setTimeout(() => {
+											uni.navigateBack({});
+										}, 1000);
+									}
+								})
+								.catch(err => {
+									uni.hideLoading();
+									uni.showToast({
+										title: '提交失败，请重新提交',
+										icon: 'none'
+									});
+								});
+						});
+				} else {
+					var obj = this.mineMsg;
+					this.$api
+						.changeUserMsg(obj)
+						.then(res => {
+							uni.hideLoading();
+							if (res.code == 20000) {
+								uni.showToast({
+									title: '修改成功',
+									duration: 2000
+								});
+								setTimeout(() => {
+									uni.navigateBack({});
+								}, 1000);
+							}
+						})
+						.catch(err => {
+							uni.hideLoading();
+							uni.showToast({
+								title: '提交失败，请重新提交',
+								icon: 'none'
+							});
+						});
+				}
+				// } else {
+				// 	uni.showToast({
+				// 		title: this.judge().value,
+				// 		icon: 'none'
+				// 	});
+				// 	return false;
+				// }
+			},
+			isPoneAvailable(poneInput) {
+				var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
+				if (!myreg.test(poneInput)) {
+					return false;
+				} else {
+					return true;
+				}
+			},
+			avatarChoose() {
+				let that = this;
+				uni.chooseImage({
+					count: 1,
+					sizeType: ['original', 'compressed'],
+					sourceType: ['album', 'camera'],
+					success(res) {
+						const tempFilePaths = res.tempFilePaths;
+						that.temp = tempFilePaths;
+						that.mineMsg.img = tempFilePaths[0];
+					}
+				});
+			}
 		}
-	}
-};
+	};
 </script>
 
 <style lang="scss">
-.body {
-	width: 100%;
-}
-.container {
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-start;
-	align-items: center;
+	.body {
+		width: 100%;
+	}
 
-	.ui-all {
+	.container {
 		width: 100%;
 		display: flex;
 		flex-direction: column;
 		justify-content: flex-start;
 		align-items: center;
 
-		.avatar {
-			width: 100%;
-			height: 180rpx;
-			border-bottom: solid 1px #f2f2f2;
-			display: flex;
-			flex-direction: row;
-			justify-content: flex-start;
-			align-items: center;
-
-			.imgAvatar {
-				width: 140rpx;
-				height: 140rpx;
-				border-radius: 50%;
-				display: inline-block;
-				vertical-align: middle;
-				overflow: hidden;
-				margin-left: 50rpx;
-
-				.iavatar {
-					width: 100%;
-					height: 100%;
-					display: block;
-				}
-			}
-
-			text {
-				display: inline-block;
-				vertical-align: middle;
-				color: #8e8e93;
-				font-size: 28rpx;
-				margin-left: 40rpx;
-			}
-		}
-
-		.text-box {
-			width: 100%;
-			height: 100rpx;
-			display: flex;
-			flex-direction: row;
-			justify-content: flex-start;
-			align-items: center;
-			border-bottom: solid 1px #f2f2f2;
-
-			text {
-				display: inline-block;
-				vertical-align: middle;
-				color: #8e8e93;
-				font-size: 28rpx;
-				margin-left: 30rpx;
-				width: 150rpx;
-			}
-
-			.input-box {
-				color: rgb(121, 121, 121);
-				margin-left: 20rpx;
-				font-size: 28rpx;
-			}
-
-			.place {
-				color: rgb(200, 200, 200);
-			}
-		}
-		.text-box:hover {
-			background-color: #eaeaea;
-		}
-		.textarea-box {
+		.ui-all {
 			width: 100%;
 			display: flex;
 			flex-direction: column;
 			justify-content: flex-start;
-			align-items: flex-start;
-			border-bottom: solid 1px #f2f2f2;
-
-			text {
-				display: flex;
-				justify-content: flex-start;
-				align-items: center;
-				color: #8e8e93;
-				font-size: 28rpx;
-				margin-left: 30rpx;
-				width: 150rpx;
-				height: 100rpx;
-			}
-
-			.textarea {
-				color: rgb(121, 121, 121);
-				margin-left: 20rpx;
-				font-size: 28rpx;
-				height: 150rpx;
-				width: 95%;
-				border: solid 1px #f2f2f2;
-			}
-
-			.place {
-				color: rgb(200, 200, 200);
-			}
-		}
-
-		.picker-box {
-			width: 100%;
-			height: 100rpx;
-			display: flex;
-			display: flex;
-			flex-direction: row;
-			justify-content: flex-start;
 			align-items: center;
-			border-bottom: solid 1px #f2f2f2;
 
-			text {
-				display: inline-block;
-				vertical-align: middle;
-				color: #8e8e93;
-				font-size: 28rpx;
-				margin-left: 30rpx;
-				width: 150rpx;
-			}
-
-			.picker {
-				width: 80%;
-				height: 100rpx;
-			}
-
-			.picker-text {
-				color: rgb(121, 121, 121);
-				margin-left: 26rpx;
-				height: 100rpx;
+			.avatar {
+				width: 100%;
+				height: 180rpx;
+				border-bottom: solid 1px #f2f2f2;
 				display: flex;
+				flex-direction: row;
 				justify-content: flex-start;
 				align-items: center;
+
+				.imgAvatar {
+					width: 140rpx;
+					height: 140rpx;
+					border-radius: 50%;
+					display: inline-block;
+					vertical-align: middle;
+					overflow: hidden;
+					margin-left: 50rpx;
+
+					.iavatar {
+						width: 100%;
+						height: 100%;
+						display: block;
+					}
+				}
+
+				text {
+					display: inline-block;
+					vertical-align: middle;
+					color: #8e8e93;
+					font-size: 28rpx;
+					margin-left: 40rpx;
+				}
+			}
+
+			.text-box {
+				width: 100%;
+				height: 100rpx;
+				display: flex;
+				flex-direction: row;
+				justify-content: flex-start;
+				align-items: center;
+				border-bottom: solid 1px #f2f2f2;
+
+				text {
+					display: inline-block;
+					vertical-align: middle;
+					color: #8e8e93;
+					font-size: 28rpx;
+					margin-left: 30rpx;
+					width: 150rpx;
+				}
+
+				.input-box {
+					color: rgb(121, 121, 121);
+					margin-left: 20rpx;
+					font-size: 28rpx;
+				}
+
+				.place {
+					color: rgb(200, 200, 200);
+				}
+			}
+
+			.text-box:hover {
+				background-color: #eaeaea;
+			}
+
+			.textarea-box {
+				width: 100%;
+				display: flex;
+				flex-direction: column;
+				justify-content: flex-start;
+				align-items: flex-start;
+				border-bottom: solid 1px #f2f2f2;
+
+				text {
+					display: flex;
+					justify-content: flex-start;
+					align-items: center;
+					color: #8e8e93;
+					font-size: 28rpx;
+					margin-left: 30rpx;
+					width: 150rpx;
+					height: 100rpx;
+				}
+
+				.textarea {
+					color: rgb(121, 121, 121);
+					margin-left: 20rpx;
+					font-size: 28rpx;
+					height: 150rpx;
+					width: 95%;
+					border: solid 1px #f2f2f2;
+				}
+
+				.place {
+					color: rgb(200, 200, 200);
+				}
+			}
+
+			.picker-box {
+				width: 100%;
+				height: 100rpx;
+				display: flex;
+				display: flex;
+				flex-direction: row;
+				justify-content: flex-start;
+				align-items: center;
+				border-bottom: solid 1px #f2f2f2;
+
+				text {
+					display: inline-block;
+					vertical-align: middle;
+					color: #8e8e93;
+					font-size: 28rpx;
+					margin-left: 30rpx;
+					width: 150rpx;
+				}
+
+				.picker {
+					width: 80%;
+					height: 100rpx;
+				}
+
+				.picker-text {
+					color: rgb(121, 121, 121);
+					margin-left: 26rpx;
+					height: 100rpx;
+					display: flex;
+					justify-content: flex-start;
+					align-items: center;
+				}
+			}
+
+			.picker-box:hover {
+				background-color: #eaeaea;
 			}
 		}
-		.picker-box:hover {
-			background-color: #eaeaea;
+
+		.save {
+			width: 80%;
+			background: #030303;
+			border: none;
+			color: #ffffff;
+			margin: 100rpx 0rpx;
+			font-size: 28rpx;
 		}
 	}
-
-	.save {
-		width: 80%;
-		background: #030303;
-		border: none;
-		color: #ffffff;
-		margin: 100rpx 0rpx;
-		font-size: 28rpx;
-	}
-}
 </style>
