8b232ee8a9aca2798f18623395db580cc7364e13
This-kiki <66477598+This-kiki@users.noreply.github.com>
2021-09-29 11:41:10 +0800

    update'

--- Commerce/pages/Activity/ActivityInfo/ActivityInfo.vue
+++ Commerce/pages/Activity/ActivityInfo/ActivityInfo.vue
@@ -26,6 +26,54 @@
 					&#xe8ab;
 				</view>
 			</view>
+			<view class="content-title">
+				{{ dataForm.name }}
+			</view>
+			<view class="content-msgs">
+				<view class="content-msgs-item">
+					<view class="iconfont content-msgs-item-icon">
+						&#xe8c5;
+					</view>
+					<view class="content-msgs-item-key">
+						活动时间:
+					</view>
+					<view class="content-msgs-item-value">
+						{{ dataForm.modifiedTime }}
+					</view>
+				</view>
+				<u-line color="rgb(222, 222, 222)" style="width: 100%;"></u-line>
+				<view class="content-msgs-item">
+					<view class="iconfont content-msgs-item-icon">
+						&#xe67e;
+					</view>
+					<view class="content-msgs-item-key">
+						活动地点:
+					</view>
+					<view class="content-msgs-item-value">
+						北京体育馆一楼体育中心
+					</view>
+				</view>
+				<u-line color="rgb(222, 222, 222)" style="width: 100%;"></u-line>
+				<view class="content-msgs-item">
+					<view class="iconfont content-msgs-item-icon">
+						&#xe8c3;
+					</view>
+					<view class="content-msgs-item-key">
+						联系方式:
+					</view>
+					<view class="content-msgs-item-value">
+						14752526623
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="contenttext">
+			<view class="contenttext-content">
+				<view class="contenttext-content-name">
+					活动详情
+				</view>
+				<view class="contenttext-content-value" v-html="dataForm.content"></view>
+			</view>
 		</view>
 		<u-toast ref="uToast" />
 	</view>
@@ -99,6 +147,7 @@ export default {
 
 <style lang="scss" scoped>
 	.body {
+		background-color: rgb(248,248,248);
 		.backImg {
 			height: 400rpx;
 			width: 100%;
@@ -109,10 +158,9 @@ export default {
 		}
 		.content {
 			margin: 0 auto;
-			width: 90%;
-			height: 1500rpx;
+			width: 92%;
 			background-color: #ffffff;
-			box-shadow: 0px 0px 4px rgb(111, 111, 111);
+			box-shadow: 0px 0px 2px rgb(111, 111, 111);
 			position: relative;
 			top: -150rpx;
 			.content-reaction {
@@ -140,6 +188,63 @@ export default {
 					color: rgb(171, 171, 171);
 				}
 			}
+			.content-title {
+				margin: 0 auto;
+				width: 94%;
+				font-size: 35rpx;
+				font-weight: 1000;
+			}
+			.content-msgs {
+				width: 100%;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				margin: 20rpx 0;
+				.content-msgs-item {
+					display: flex;
+					flex-direction: row;
+					margin: 0 auto;
+					width: 90%;
+					height: 80rpx;
+					align-items: center;
+					.content-msgs-item-icon {
+						font-size: 38rpx;
+						color: rgb(182, 182, 182);
+					}
+					.content-msgs-item-key {
+						font-size: 28rpx;
+						color: rgb(191, 191, 191);
+						margin: 0 15rpx 0 5rpx;
+					}
+					.content-msgs-item-value {
+						font-size: 27rpx;
+						color: rgb(79, 79, 79);
+					}
+				}
+			}
+		}
+		.contenttext {
+			margin: 0 auto;
+			width: 92%;
+			top: -140rpx;
+			background-color: #ffffff;
+			box-shadow: 0px 0px 2px rgb(111, 111, 111);
+			position: relative;
+			padding: 30rpx 0;
+			.contenttext-content {
+				width: 94%;
+				margin: 0 auto;
+				.contenttext-content-name {
+					font-size: 28rpx;
+					color: rgb(159, 159, 159);
+					margin: 10rpx 0 60rpx 0;
+				}
+				.contenttext-content-value {
+					font-size: 30rpx;
+				}
+			}
+			
 		}
 	}
 </style>
