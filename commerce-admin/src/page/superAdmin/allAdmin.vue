13fc40a9d828a78249ea2f83f0742a6ac823730c
This-kiki <66477598+This-kiki@users.noreply.github.com>
2022-02-17 11:40:02 +0800

    add

--- commerce-admin/src/page/superAdmin/allAdmin.vue
+++ commerce-admin/src/page/superAdmin/allAdmin.vue
@@ -28,7 +28,7 @@
       </el-table-column>
       <el-table-column fixed="right" label="操作" width="150" align="center">
         <template slot-scope="scope">
-          <el-button type="primary" plain circle @click="editAdmin(scope.row)" icon="el-icon-edit-outline" size="small"></el-button>
+          <!-- <el-button type="primary" plain circle @click="editAdmin(scope.row)" icon="el-icon-edit-outline" size="small"></el-button> -->
           <el-popconfirm confirm-button-text='好的' cancel-button-text='取消' icon="el-icon-info" icon-color="red" title="确定删除该管理员吗" @confirm="deleteAdmin(scope.row)">
             <el-button slot="reference" type="danger" plain circle icon="el-icon-delete" size="small"></el-button>
           </el-popconfirm>
@@ -45,63 +45,63 @@ export default {
       tableData: [],
       addForm: {},
       addVisible: false,
-    }
+    };
   },
   mounted() {
-    this.getAllAdmin()
+    this.getAllAdmin();
   },
   methods: {
     getAllAdmin() {
       this.$http.getGeneralAdmin().then((res) => {
         // console.log(res)
-        var resp = res.data.adminUser
+        var resp = res.data.adminUser;
         for (let i = 0; i < resp.length; i++) {
           if (resp[i].groupid == 0) {
-            resp[i].identity = '超级管理员'
+            resp[i].identity = "超级管理员";
           } else if (resp[i].groupid == 1) {
-            resp[i].identity = '普通管理员'
+            resp[i].identity = "普通管理员";
           }
         }
-        this.tableData = resp
-      })
+        this.tableData = resp;
+      });
     },
     deleteAdmin(row) {
       // console.log(row)
-      var postAPI = { id: row.id }
+      var postAPI = { id: row.id };
       this.$http.deleteGeneralAdmin(postAPI).then((res) => {
         this.$message({
-          message: '删除成功',
-          type: 'success',
-        })
-        this.getAllAdmin()
-      })
+          message: "删除成功",
+          type: "success",
+        });
+        this.getAllAdmin();
+      });
     },
     addAdmin() {
-      const postAPI = this.addForm
+      const postAPI = this.addForm;
       if (!postAPI.password || postAPI.password.length <= 5) {
         this.$notify.info({
-          title: '密码',
-          message: '请输入至少6位的密码',
-        })
+          title: "密码",
+          message: "请输入至少6位的密码",
+        });
       } else if (!postAPI.username) {
         this.$notify.info({
-          title: '用户名',
-          message: '请输入用户名',
-        })
+          title: "用户名",
+          message: "请输入用户名",
+        });
       } else {
-        console.log(this.addForm)
+        console.log(this.addForm);
         this.$http.addGeneralAdmin(postAPI).then((res) => {
           this.$message({
-            message: '添加成功',
-            type: 'success',
-          })
-          this.getAllAdmin()
-          this.addVisible = false
-        })
+            message: "添加成功",
+            type: "success",
+          });
+          this.getAllAdmin();
+          this.addVisible = false;
+        });
       }
     },
   },
-}
+};
 </script>
 <style scoped>
 </style>
