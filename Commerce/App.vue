f1e52a8276b596adda9de15923e69dc3493af9e9
This-kiki <66477598+This-kiki@users.noreply.github.com>
2022-02-21 18:28:52 +0800

    Update App.vue
    add

--- Commerce/App.vue
+++ Commerce/App.vue
@@ -15,4 +15,25 @@
 <style lang="scss">
 	/*每个页面公共css */
 	@import "@/uni_modules/uview-ui/index.scss";
+	uni-picker-view {
+	  display: block;
+	}
+	
+	uni-picker-view .uni-picker-view-wrapper {
+	  display: flex;
+	  position: relative;
+	  overflow: hidden;
+	  height: 100%;
+	  background-color: white;
+	}
+	
+	uni-picker-view[hidden] {
+	  display: none;
+	}
+	
+	picker-view {
+	    width: 100%;
+	    height: 600upx;
+	    margin-top:20upx;
+	}
 </style>
